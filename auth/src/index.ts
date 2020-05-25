a9ef118c6242e865f96e70a32d08072f328f9af3
Muthu Venkatachalam <muthukarupasamy.venkatachalam@resideo.com>
2020-05-25 18:59:30 +0530

    added prereq for test framework

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,35 +1,5 @@
-import express from "express";
-import { json } from "body-parser";
-import "express-async-errors";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-import { signinRouter } from "./routes/signin";
-import { signupRouter } from "./routes/signup";
-import { meRouter } from "./routes/whoami";
-import { errorHandler } from "./middlewares/error-handler";
-import { BadRequestError } from "./errors/bad-request-error";
-import { signoutRouter } from "./routes/signout";
-
-const app = express();
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: false,
-  })
-);
-
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(meRouter);
-app.use(signoutRouter);
-
-app.all("*", () => {
-  throw new BadRequestError("Invalid Route");
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
